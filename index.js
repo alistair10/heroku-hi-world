5c5eb5dc26d590b2fcf174a4882801f7781e823a
Alistair Greener <alistairgreener@Alistairs-MacBook-Pro.local>
2019-04-10 17:12:03 +0100

    changed message

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Hi world, made some changes\n');
 });
 server.listen(port, () => {
   console.log(`Server running on ${port}`);
