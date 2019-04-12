2c3aac24e7276ab6486fbee65ec7f31ed1c8051c
Alistair Greener <alistairgreener@alistairs-mbp.lan>
2019-04-12 20:06:57 +0100

    altered message

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const http = require('http');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<span onclick="function click() { console.log("clicked"); }">click us man</span>\n');
+  res.end('<b onclick="document.querySelector(`b`).innerHTML = `wanker!`;"  style="font-size: 50">Press on us man!!</b>\n');
 });
 server.listen(port, () => {
   console.log(`Server running on ${port}`);
