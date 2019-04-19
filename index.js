1e03b5a80d190da4acbc50d418553cf6046bf11a
Alistair Greener <alistairgreener@alistairs-mbp.lan>
2019-04-19 18:58:15 +0100

    db changes

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const http = require('http'), mysql2 = require('mysql2');
 
 const port = process.env.PORT || 4000;
 
+function run_query(connection, query) {
+	return new Promise((res,rej)=> {
+		connection.execute(query, (err,result)=> {
+			err ? rej(err) : res(result);
+		})
+	});
+}
+
 const conn_obj = {
 	host: 'us-cdbr-iron-east-02.cleardb.net',
 	user: 'bf1c235e8d2c83',
@@ -9,13 +17,14 @@ const conn_obj = {
 	password: '0cf703e4',
 };
 
-const conn = mysql2.createConnection(conn_obj);
-
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('hi');
-});  
+  const conn = mysql2.createConnection(conn_obj);
+  run_query(conn, "SELECT * FROM test")
+  	.then(msg => res.end(`msg: ${msg[0].id}`))
+  	.catch(err => res.end(`err: ${err}`));
+});
 
 server.listen(port, () => {
   console.log(`Server running on ${port}`);
